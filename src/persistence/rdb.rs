0822a8cbc1e18b4fbaaed2443ed7c4c4aeecf6ee
wangle <wangle@agree.com.cn>
2024-09-02 11:15:04 +0800

    fix:  修复RDB文件打开错误信息
    - 精简代码导入，改进文件处理和进度条模板。
    - 修复RDB文件打开错误信息
    - 格式化代码

--- src/persistence/rdb.rs
+++ src/persistence/rdb.rs
@@ -1,11 +1,7 @@
 use std::fs::File;
 use std::io::Seek;
 use std::io::Write;
-use std::{
-    fs::OpenOptions,
-    io::SeekFrom,
-    sync::Arc
-};
+use std::{fs::OpenOptions, io::SeekFrom, sync::Arc};
 
 use parking_lot::Mutex;
 
@@ -25,14 +21,17 @@ pub struct Rdb {
 }
 
 impl Rdb {
-    
     pub fn new(rudis_config: Arc<RudisConfig>, db: Arc<Mutex<Db>>) -> Rdb {
         let mut rdb_file = None;
         if let Some(filename) = &rudis_config.dbfilename {
             let base_path = &rudis_config.dir;
             let file_path = format!("{}{}", base_path, filename);
             rdb_file = Some(
-                OpenOptions::new().create(true).write(true).open(file_path).expect("Failed to open AOF file"),
+                OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .open(file_path)
+                    .expect("Failed to open RDB file"),
             );
         }
 
@@ -60,19 +59,34 @@ impl Rdb {
                     let expire_at = redis_data.get_expire_at();
                     let protocol_line = match redis_data.get_value() {
                         TimedDataValue::List(list) => {
-                            format!("{}\\r\\n{}\\r\\n{:?}\\r\\nList\\r\\n{}",db_index, key, list, expire_at)
+                            format!(
+                                "{}\\r\\n{}\\r\\n{:?}\\r\\nList\\r\\n{}",
+                                db_index, key, list, expire_at
+                            )
                         }
                         TimedDataValue::Hash(hash) => {
-                            format!("{}\\r\\n{}\\r\\n{:?}\\r\\nHash\\r\\n{}", db_index, key, hash, expire_at)
+                            format!(
+                                "{}\\r\\n{}\\r\\n{:?}\\r\\nHash\\r\\n{}",
+                                db_index, key, hash, expire_at
+                            )
                         }
                         TimedDataValue::Zset(zset) => {
-                            format!("{}\\r\\n{}\\r\\n{:?}\\r\\nZset\\r\\n{}", db_index, key, zset, expire_at)
+                            format!(
+                                "{}\\r\\n{}\\r\\n{:?}\\r\\nZset\\r\\n{}",
+                                db_index, key, zset, expire_at
+                            )
                         }
                         TimedDataValue::String(value) => {
-                            format!("{}\\r\\n{}\\r\\n{}\\r\\nString\\r\\n{}", db_index, key, value, expire_at)
+                            format!(
+                                "{}\\r\\n{}\\r\\n{}\\r\\nString\\r\\n{}",
+                                db_index, key, value, expire_at
+                            )
                         }
                         TimedDataValue::Set(set) => {
-                            format!("{}\\r\\n{}\\r\\n{:?}\\r\\nSet\\r\\n{}", db_index, key, set, expire_at)
+                            format!(
+                                "{}\\r\\n{}\\r\\n{:?}\\r\\nSet\\r\\n{}",
+                                db_index, key, set, expire_at
+                            )
                         }
                     };
                     if let Err(err) = writeln!(file, "{}", protocol_line) {
@@ -97,32 +111,40 @@ impl Rdb {
                     let pb = ProgressBar::new(line_count);
                     pb.set_style(
                         ProgressStyle::default_bar()
-                            .template("[{bar:39.green/cyan}] percent: {percent}% lines: {pos}/{len}")
+                            .template(
+                                "[{bar:39.green/cyan}] percent: {percent}% lines: {pos}/{len}",
+                            )
                             .progress_chars("=>-"),
                     );
                     let reader = BufReader::new(&mut file);
                     for line in reader.lines() {
                         if let Ok(operation) = line {
-                            
                             let parts: Vec<&str> = operation.split("\\r\\n").collect();
                             let data_type = parts[3].to_string();
                             let db_index = parts[0].parse::<usize>().unwrap();
                             let expire_at = parts[4].parse().unwrap();
                             let value_str = parts[2];
                             let value: Option<TimedDataValue> = match data_type.as_str() {
-                                "List" => Some(TimedDataValue::List(serde_json::from_str(value_str).unwrap())),
-                                "Hash" => Some(TimedDataValue::Hash(serde_json::from_str(value_str).unwrap())),
-                                "Zset" => Some(TimedDataValue::Zset(serde_json::from_str(value_str).unwrap())),
+                                "List" => Some(TimedDataValue::List(
+                                    serde_json::from_str(value_str).unwrap(),
+                                )),
+                                "Hash" => Some(TimedDataValue::Hash(
+                                    serde_json::from_str(value_str).unwrap(),
+                                )),
+                                "Zset" => Some(TimedDataValue::Zset(
+                                    serde_json::from_str(value_str).unwrap(),
+                                )),
                                 "String" => Some(TimedDataValue::String(value_str.to_string())),
-                                "Set" => Some(TimedDataValue::Set(serde_json::from_str(value_str).unwrap())),
-                                _ => None
+                                "Set" => Some(TimedDataValue::Set(
+                                    serde_json::from_str(value_str).unwrap(),
+                                )),
+                                _ => None,
                             };
 
                             if let Some(redis_value) = value {
                                 // Handle the TimedDataValue here
                                 let key = parts[1].to_string();
                                 db_ref.set(db_index, key, redis_value, expire_at);
-                                    
                             }
                         }
                         pb.inc(1);
