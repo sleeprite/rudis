a9d7233f99494c72cbaef7dd2120afb42906b8f0
就眠儀式 <854085467@qq.com>
2025-01-08 17:21:30 +0800

    update

--- src/cmd/string/append.rs
+++ src/cmd/string/append.rs
@@ -3,7 +3,7 @@ use crate::{db::{Db, Structure}, frame::Frame};
 
 pub struct Append {
     key: String,
-    value: String,
+    val: String,
 }
 
 impl Append {
@@ -11,18 +11,18 @@ impl Append {
     pub fn parse_from_frame(frame: Frame) -> Result<Self, Error> {
 
         let key = frame.get_arg(1);
-        let value = frame.get_arg(2);
+        let val = frame.get_arg(2);
 
-        if key.is_none() || value.is_none() {
+        if key.is_none() || val.is_none() {
             return Err(Error::msg("ERR wrong number of arguments for 'append' command"));
         }
 
         let key_str = key.unwrap().to_string(); // 键
-        let value_str = value.unwrap().to_string(); // 值
+        let val_str = val.unwrap().to_string(); // 值
 
         Ok(Append {
             key: key_str,
-            value: value_str,
+            val: val_str,
         })
     }
 
@@ -34,7 +34,7 @@ impl Append {
             None => &String::new(),
         };
 
-        let new_value = format!("{}{}", existing_value, self.value);
+        let new_value = format!("{}{}", existing_value, self.val);
         db.insert(self.key, Structure::String(new_value));
 
         Ok(Frame::Ok)
