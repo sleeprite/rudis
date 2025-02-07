04c264cc8bfad9ee476ecbd0d6bf3e57598c9527
就眠儀式 <854085467@qq.com>
2025-02-07 11:33:08 +0800

    修改 echo 命令

--- src/cmd/echo.rs
+++ src/cmd/echo.rs
@@ -3,22 +3,22 @@ use anyhow::Error;
 use crate::frame::Frame;
 
 pub struct Echo {
-    message: String,
+    str: String,
 }
 
 impl Echo {
 
     pub fn parse_from_frame(frame: Frame) -> Result<Self, Error> {
-        let message = match frame.get_arg(1) {
+        let str = match frame.get_arg(1) {
             Some(name) => name.to_string(),
             None => return Err(Error::msg("ERR wrong number of arguments for 'echo' command")),
         };
         Ok(Echo {
-            message
+            str
         })   
     }
 
     pub fn apply(self) -> Result<Frame, Error> {
-        Ok(Frame::BulkString(self.message))
+        Ok(Frame::BulkString(self.str))
     }
 }
